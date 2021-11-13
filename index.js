747094876243ad33e7ebf9b73e70a924ae65204b
ShimulAUST <shimulpaul59@gmail.com>
2021-11-13 11:45:25 +0600

    Update order status

--- index.js
+++ index.js
@@ -9,6 +9,20 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+async function verifyToken(req, res, next) {
+    if (req.headers.authorization?.startsWith('Bearer ')) {
+        const token = req.headers.authorization.split(' ')[1];
+        try {
+            const decodedUser = await admin.auth().verifyIdToken(token);
+            req.decodedEmail = decodedUser.email;
+        }
+        catch {
+
+        }
+    }
+    next();
+}
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.da9dr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
@@ -51,6 +65,22 @@ async function run() {
             const result = await ordersCollection.deleteOne(query);
             res.json(result);
         });
+        //update order status 
+        app.put('/orders/:id', async (req, res) => {
+            const id = req.params.id;
+            const updateUser = "approved";
+            const filter = { _id: ObjectId(id) };
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: {
+                    status: updateUser
+                },
+            };
+            const result = await ordersCollection.updateOne(filter, updateDoc, options);
+
+            res.send(result);
+        })
+
         app.post('/reviews', async (req, res) => {
             const review = req.body;
             const result = await reviewsCollection.insertOne(review);
